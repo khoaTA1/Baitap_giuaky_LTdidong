005d63f5dfac6a85b7fa23a5392b1f5c37ed4522
khoaTA1 <khoatdmk2@gmail.com>
2025-11-26 18:28:53 +0700

    Cập nhật greeting trên layout/activity home, sửa nút back trong setting

--- app/src/main/java/com/example/bt1/activities/SettingActivity.java
+++ app/src/main/java/com/example/bt1/activities/SettingActivity.java
@@ -1,10 +1,12 @@
 package com.example.bt1.activities;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
@@ -12,10 +14,11 @@ import androidx.core.content.ContextCompat;
 
 import com.example.bt1.R;
 import com.example.bt1.utils.Notify;
+import com.google.android.material.materialswitch.MaterialSwitch;
 import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class SettingActivity extends AppCompatActivity {
-    private SwitchMaterial darkThemeSwitch, notifyEnableSwitch, vibrateOnNotifySwitch;
+    private MaterialSwitch darkThemeSwitch, notifyEnableSwitch, vibrateOnNotifySwitch;
     private ImageView backButton;
     private SharedPreferences prefs;
 
@@ -34,13 +37,22 @@ public class SettingActivity extends AppCompatActivity {
         setContentView(R.layout.setting);
 
         backButton = findViewById(R.id.back_button);
-        backButton.setOnClickListener(v -> {
-            finish();
-        });
 
         darkThemeSwitch = findViewById(R.id.dark_theme_switch);
         notifyEnableSwitch = findViewById(R.id.notify_enable_switch);
         vibrateOnNotifySwitch = findViewById(R.id.enable_vibrate_on_notify_switch);
+
+        // Đăng ký callback khi nhấn nút quay lại
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                backBtnProcess();
+            }
+        });
+
+        backButton.setOnClickListener(v -> {
+            backBtnProcess();
+        });
 
         // áp dụng lại màu thumb sau khi recreate activity
         updateSwitchColor(darkThemeSwitch, isDark);
@@ -87,8 +99,14 @@ public class SettingActivity extends AppCompatActivity {
     }
 
     // Hàm cập nhật màu của switch dựa trên trạng thái bật/tắt
-    private void updateSwitchColor(SwitchMaterial sw, boolean isChecked) {
+    private void updateSwitchColor(MaterialSwitch sw, boolean isChecked) {
         int thumbColor = isChecked ? R.color.thumb_enable_color : R.color.thumb_disable_color;
         sw.setThumbTintList(ColorStateList.valueOf(ContextCompat.getColor(this, thumbColor)));
+    }
+
+    private void backBtnProcess() {
+        Intent intent = new android.content.Intent(this, HomeActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
